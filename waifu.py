c6a5960a8152d5c3615dfd747daa59d3a6b687ac
TrueSaiyan <ufperth@protonmail.com>
2024-08-12 01:01:28 +0800

    missing import

--- waifu.py
+++ waifu.py
@@ -8,8 +8,9 @@
 """
 
 import re
+import random  # Import random module
 
-from . import *
+from . import *  # Ensure this imports necessary components
 
 EMOJI_PATTERN = re.compile(
     "["
@@ -27,18 +28,15 @@ EMOJI_PATTERN = re.compile(
     "]+",
 )
 
-
 def deEmojify(inputString: str) -> str:
     """Remove emojis and other non-safe characters from string"""
     return re.sub(EMOJI_PATTERN, "", inputString)
-
 
 @ultroid_cmd(
     pattern="waifu ?(.*)",
 )
 async def waifu(animu):
     xx = await eor(animu, get_string("com_1"))
-    # """Creates random anime sticker!"""
     text = animu.pattern_match.group(1)
     if not text:
         if animu.is_reply:
@@ -46,13 +44,14 @@ async def waifu(animu):
         else:
             await xx.edit(get_string("sts_1"))
             return
+    
     waifus = [32, 33, 37, 40, 41, 42, 58, 20]
-    finalcall = "#" + (str(random.choice(waifus)))
-    sticcers = await animu.client.inline_query(
+    finalcall = "#" + str(random.choice(waifus))
+    stickers = await animu.client.inline_query(
         "stickerizerbot",
-        f"{finalcall}{(deEmojify(text))}",
+        f"{finalcall}{deEmojify(text)}",
     )
-    await sticcers[0].click(
+    await stickers[0].click(
         animu.chat_id,
         reply_to=animu.reply_to_msg_id,
         silent=bool(animu.is_reply),
